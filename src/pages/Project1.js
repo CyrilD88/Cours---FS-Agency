418af96ec95c89c0f2c608539338c2751831afac
Cyril Derc <cyril_derc@hotmail.com>
2023-07-16 14:50:26 +0200

    PAGE 1 - first commit de la page 1

--- src/pages/Project1.js
+++ src/pages/Project1.js
@@ -1,10 +1,19 @@
 import React from "react";
+import Mouse from "../components/Mouse";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+import Buttons from "../components/Buttons";
 
 const Project1 = () => {
   return (
-    <div>
-      <h1>Project 1</h1>
-    </div>
+    <main>
+      <Mouse />
+      <div className="project">
+        <Navigation />
+        <Logo />
+        <Buttons left={"/"} right={"/projet-2"} />
+      </div>
+    </main>
   );
 };
 
