06336d06a4fa54c3d5ede0e6a8bd577bb2c783c5
Cyril Derc <cyril_derc@hotmail.com>
2023-07-16 18:58:03 +0200

    dernier commit

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,65 @@
 import React from "react";
+import Mouse from "../components/Mouse";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+import ContactForm from "../components/ContactForm";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+// penser à télécharger react-copy-to-clipboard
+import SocialNetwork from "../components/SocialNetwork";
+import Buttons from "../components/Buttons";
 
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact</h1>
-    </div>
+    <main>
+      <Mouse />
+      <div className="contact">
+        <Navigation />
+        <Logo />
+        <ContactForm />
+        <div className="contact-infos">
+          <div className="address">
+            <div className="content">
+              <h4>adresse</h4>
+              <p>12 rue Laplace</p>
+              <p>64200 Biarritz</p>
+            </div>
+          </div>
+          <div className="phone">
+            <div className="content">
+              <h4>téléphone</h4>
+              <CopyToClipboard text="0666563738" className="hover">
+                <p
+                  style={{ cursor: "pointer" }}
+                  className="clipboard"
+                  onClick={() => alert("Téléphone copié")}
+                >
+                  06 66 56 37 38
+                </p>
+              </CopyToClipboard>
+            </div>
+          </div>
+          <div className="email">
+            <div className="content">
+              <h4>email</h4>
+              <CopyToClipboard text="fsagency@gmail.com" className="hover">
+                <p
+                  style={{ cursor: "pointer" }}
+                  className="clipboard"
+                  onClick={() => alert("email copié")}
+                >
+                  fsagency@gmail.com
+                </p>
+              </CopyToClipboard>
+            </div>
+          </div>
+          <SocialNetwork />
+          <div className="credits">
+            <p>Cyril - 2023</p>
+          </div>
+        </div>
+        <Buttons left={"/projet-4"} />
+      </div>
+    </main>
   );
 };
 
